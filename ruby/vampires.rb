0acfdc62a96564b6e410d280f8c9d3b1d882b053
FridaSjoholm <frida@fridaesign.se>
2016-10-03 19:26:56 -0700

    Adds questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,15 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp
+
+puts "What year where you born?"
+year_of_birth = gets.chomp 
+
+puts "Our company cafeteria serves garlic bread, do you want some? (Y/N)"
+bread_liking = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance? (Y/N)"
+insurance = gets.chomp
+ 
