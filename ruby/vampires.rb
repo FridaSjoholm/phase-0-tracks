9c57d0e615cc42b3d8773eaab2283f5da9a5ce21
FridaSjoholm <frida@fridaesign.se>
2016-10-05 10:39:13 -0700

    Adds conversion to boolean values to enable easier processing

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,5 +1,5 @@
 puts "What is your name?"
-name = gets.chomp
+nam = gets.chomp
 
 puts "How old are you?"
 age = gets.chomp
@@ -7,9 +7,26 @@ age = gets.chomp
 puts "What year where you born?"
 year_of_birth = gets.chomp 
 
+if 2016- year_of_birth.to_i == age.to_i
+	mathage = true;
+else	
+	mathage = false;
+end
+
 puts "Our company cafeteria serves garlic bread, do you want some? (Y/N)"
 bread_liking = gets.chomp
+case bread_liking
+when "Y"
+	bread_liking = true;
+when "N"
+	bread_liking = false;
+end
 
 puts "Would you like to enroll in the company's health insurance? (Y/N)"
 insurance = gets.chomp
- 
+case insurance
+when "Y"
+	insurance = true;
+when "N"
+	insurance = false;
+end
