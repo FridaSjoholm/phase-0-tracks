4224846b130a44f0695096777113f11e129a86d5
FridaSjoholm <frida@fridaesign.se>
2016-10-12 08:41:42 -0700

    Adds testing of methods

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,37 +4,24 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-puts "iNvEsTiGaTiOn".swapcase
+puts "iNvEsTiGaTiOn".swapcase == "InVeStIgAtIoN"
 
-# => “InVeStIgAtIoN”
+puts "zom".sub(/[o]/, "oo") == "zoom"
 
-puts "zom".sub(/[o]/, "oo")
-# => “zoom”
+puts "enhance".ljust(11).rjust(15) == "    enhance    "
 
-p"enhance".ljust(11).rjust(15)
-# => "    enhance    "
+puts "Stop! You’re under arrest!".upcase == "STOP! YOU’RE UNDER ARREST!"
 
-puts "Stop! You’re under arrest!".upcase
-# => "STOP! YOU’RE UNDER ARREST!"
+puts "the usual".insert(-1, " suspects") == "the usual suspects"
 
-puts "the usual".insert(-1, " suspects")
-#=> "the usual suspects"
+puts " suspects".insert(0, "the usual") == "the usual suspects"
 
-puts " suspects".insert(0, "the usual")
-# => "the usual suspects"
+puts "The case of the disappearing last letter".chop == "The case of the disappearing last lette"
 
-puts "The case of the disappearing last letter".chop
-# => "The case of the disappearing last lette"
+puts "The mystery of the missing first letter"[1..-1] == "he mystery of the missing first letter"
 
-puts "The mystery of the missing first letter"[1..-1]
-# => "he mystery of the missing first letter"
+puts "Elementary,    my   dear        Watson!".squeeze == "Elementary, my dear Watson!"
 
-puts "Elementary,    my   dear        Watson!".squeeze
-# => "Elementary, my dear Watson!"
+puts "z".ord == 122 
 
-puts "z".ord
-# => 122 
-# (What is the significance of the number 122 in relation to the character z?)
-
-puts "How many times does the letter 'a' appear in this string?".count "a"
-# => 4
+puts ("How many times does the letter 'a' appear in this string?".count "a") == 4
