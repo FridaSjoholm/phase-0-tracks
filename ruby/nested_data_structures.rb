da1ca9b3aa42500b1c65e12c72f3a6237c0fcf4c
FridaSjoholm <frida@fridaesign.se>
2016-10-12 22:44:50 -0700

    Prints info from nested elements

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -32,5 +32,18 @@ recipes = {
 		"butter",
 		"flour",
 	]
-
 }
+
+puts "The ingredients in a apple pie is:"
+puts recipes["Apple pie"][0..-1]
+
+puts "Here! have some salt:"
+puts recipes["Butternut squash soup"][5]
+puts recipes["Caprese salad"][4]
+puts recipes["Swedish pancakes"][4]
+
+puts "these recipies have flour in them:"
+puts "Butternut squash soup: #{recipes["Butternut squash soup"].include?("flour")}"
+puts "Caprese salad: #{recipes["Caprese salad"].include?("flour")}"
+puts "Swedish pancakes: #{recipes["Swedish pancakes"].include?("flour")}"
+puts "Apple pie: #{recipes["Apple pie"].include?("flour")}"
