b3e979d043be545323b7d43e7df032707eede762
FridaSjoholm <frida@fridaesign.se>
2016-10-17 13:30:43 -0700

    Adds charasteristics and behavior in shape of comments

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+#SPECIES ------------------------
+#Puppy
+
+#CHARACTERISTICS ----------------
+#Height: 20 inches
+#Eyecolor: green
+#Friendly: yes
+#Fur length: 10 inches
+#Color: Ginger
+
+
+#BEHAVIOR -----------------------
+#Snuggle 
+#Cuddle
+#Huddle
