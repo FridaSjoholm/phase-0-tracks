beb4068451d45ee2b47b320cec6cdf0d7668f81a
FridaSjoholm <frida@fridaesign.se>
2016-10-18 21:50:10 -0700

    Adds attributes to santa class

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,8 +1,12 @@
 class Santa
-	def initialize
+	def initialize(gender, ethnicity)
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
 		puts "Initializing Santa instance"
 	end
-
+	
 	def speak
 		puts "Ho, ho, ho! Haaaappy holidays!" 
 	end
@@ -13,6 +17,6 @@ class Santa
 
 end
 
-klas = Santa.new
+klas = Santa.new("trans", "hispanic")
 klas.speak
 klas.eat_milk_and_cookies("kladdkaka")
