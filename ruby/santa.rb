a6abb1cf8a4a5a3133ead4f3bb5e95dff0e6f8bf
FridaSjoholm <frida@fridaesign.se>
2016-10-18 23:36:26 -0700

    Adds Algorithm that makes 200 santas and print valuable information. cleans up file

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,18 +1,13 @@
 class Santa
-	attr_reader :age, :ethnicity
-	attr_accessor :gender
+	attr_reader :ethnicity
+	attr_accessor :gender, :age
 
 	def initialize(gender, ethnicity)
 		@gender = gender
 		@ethnicity = ethnicity
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 		@age = 0
-		puts "Initializing Santa instance"
-	end
-
-	def print_info
-		puts "This santa identifies as :#{@gender}, #{@ethnicity}, and is #{@age} years old"
-		puts "This is the santas reindeers in order of preferrability #{@reindeer_ranking}"
+	#	puts "Initializing Santa instance"
 	end
 
 	def speak
@@ -33,22 +28,27 @@ class Santa
 	end
 end
 
-santas = []
 example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
 example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-example_genders.length.times do |i|
-  santas << Santa.new(example_genders[i], example_ethnicities[i])
+
+200.times do |new_santa|
+	new_santa = Santa.new(example_genders.sample, example_ethnicities.sample)
+	new_santa.age = rand(0..140)
+	puts "This santa idintifies as #{new_santa.gender}"
+	puts "It has the ethnicity #{new_santa.ethnicity}"
+	puts "And is #{new_santa.age} years old"
+	puts "----------------------------------------------"
 end
 
-santas.each do |santa|
-	santa.print_info
-end
+#santas.each do |santa|
+#	santa.print_info
+#end
 
-santas[0].celebrate_birthday
-santas[0].gender = ("trans")
-puts santas[0].age
-puts santas[0].ethnicity
-santas[0].print_info
-
-santas[1].get_mad_at("Dasher")
-santas[1].print_info
+#santas[0].celebrate_birthday
+#santas[0].gender = ("trans")
+#puts santas[0].age
+#puts santas[0].ethnicity
+#santas[0].print_info
+#
+#santas[1].get_mad_at("Dasher")
+#santas[1].print_info
