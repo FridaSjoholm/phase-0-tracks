b25272a8889af1c86303b6273ea81aa08f4ff38b
FridaSjoholm <frida@fridaesign.se>
2016-10-19 18:56:59 -0700

    Adds another method, happy

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -2,4 +2,11 @@ module Shout
    def self.yell_angrily(words)
     words + "!!!" + " :("
   end
+
+  def self.yelling_happily(words)
+    words + "!!!" + " :D"
+  end
 end
+
+puts Shout.yell_angrily("This sucks")
+puts Shout.yelling_happily("This rocks")
