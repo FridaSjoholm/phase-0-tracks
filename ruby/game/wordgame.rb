ef076d93c09722e5b27aff9e1cf85b3c969cebb5
FridaSjoholm <frida@fridaesign.se>
2016-10-20 12:05:47 -0700

    updates comments and creates class

--- ruby/game/wordgame.rb
+++ ruby/game/wordgame.rb
@@ -1,8 +1,14 @@
 #word guessing game
 #
-#The game with all its moves and actions is going to be a class. For each game We will need one word to work with, which will be passed in with initialization. Within it we we will have these actions:
+#The game with all its moves and actions is going to be a class. For each game We will need one word to work with, 
+#which will be passed in with initialization. Within it we we will have these actions:
 #
-#- comparison, which compares the letter to the string
-#- a counter, which makes sure the player doesntguess too many times (and calculates how many time it's allowed to do so), and if the guess has allready been made-
-#- Feedback on status, an array with as many underlines as in the word the use is trying to figure out, which gets updated to correct letters once guessed.
+#- comparison, which compares the letter to the string, and if the letter exists within the string we will update 
+#an array with underlines to show at what location in the word that letter is.
+#- a counter, which makes sure the player doesntguess too many times (and calculates how many time it's allowed to do so), and if the guess has allready been made
+#the counter method will also check if the game is finished (either by winning or guessing all letters)
 #
+
+class Game
+	
+end
