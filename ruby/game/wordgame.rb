20799f758d8bc744a9f79d08ef47be3e5d2d463c
FridaSjoholm <frida@fridaesign.se>
2016-10-19 22:48:54 -0700

    Initial commit with comments

---
+++ ruby/game/wordgame.rb
@@ -0,0 +1,8 @@
+#word guessing game
+#
+#The game with all its moves and actions is going to be a class. For each game We will need one word to work with, which will be passed in with initialization. Within it we we will have these actions:
+#
+#- comparison, which compares the letter to the string
+#- a counter, which makes sure the player doesntguess too many times (and calculates how many time it's allowed to do so), and if the guess has allready been made-
+#- Feedback on status, an array with as many underlines as in the word the use is trying to figure out, which gets updated to correct letters once guessed.
+#
