8cd19e7e0a79ebdb184b45621ffe7abf262d24c4
FridaSjoholm <frida@fridaesign.se>
2016-10-21 15:06:40 -0700

    Tests comparison

--- ruby/game/wordgame_spec.rb
+++ ruby/game/wordgame_spec.rb
@@ -7,4 +7,8 @@ describe Game do
 		expect(round.array).to eq ["_","_","_","_","_","_"]
 	end
 	
+	it "Compares if letter exist in word we are looking for, if so replaces underline with letter on correct space" do
+		expect(round.compare("c")).to eq ["_","_","_","c","_","_"]
+	end
+
 end
