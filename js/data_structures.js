55ea1a880f32d329064934c1a22f75c875ed963c
FridaSjoholm <frida@fridaesign.se>
2016-10-25 16:17:21 -0700

    Adds extra function

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,2 +1,11 @@
 var colors = ["green", "yellow", "pink", "black"];
 var names = ["Joey", "Chandler", "Rachel", "Phoebe"];
+
+console.log(colors);
+console.log(names);
+
+colors.push("purple");
+names.push("Monica");
+
+console.log(colors);
+console.log(names);
