2250d0c06a6206f7d9755bb30ed272bca62627d5
FridaSjoholm <frida@fridaesign.se>
2016-10-25 16:01:40 -0700

    Adds some inwork progress

--- js/explore.js
+++ js/explore.js
@@ -2,8 +2,13 @@
 // Iterate through string in reversed order, from back to front and  for each new letter, 
 //add it to string for reversed reversed word
 
-var original_word = "hello"
+
 var reversed_string = ""
+
+
+
+console.log("give us a word, please!")
+var original_word = gets.chomp
 
 for (var i = original_word.length-1; i >= 0; i-=1){
 	reversed_string += original_word[i]
