86b20812068a084175db9990d0c1d75f12ffbc43
FridaSjoholm <frida@fridaesign.se>
2016-10-25 15:54:14 -0700

    Adds method thet re verses word

--- js/explore.js
+++ js/explore.js
@@ -1,4 +1,12 @@
 //pseudocode: reverse word
-// Iterate throught string, for each new letter, add it first in string for new word. 
-//Return reversed word
+// Iterate through string in reversed order, from back to front and  for each new letter, 
+//add it to string for reversed reversed word
 
+var original_word = "hello"
+var reversed_string = ""
+
+for (var i = original_word.length-1; i >= 0; i-=1){
+	reversed_string += original_word[i]
+}
+
+console.log(reversed_string)
