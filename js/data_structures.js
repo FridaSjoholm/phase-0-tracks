c652a13bd9eb71c9e0000ab056b97c16d927557d
FridaSjoholm <frida@fridaesign.se>
2016-10-26 09:55:12 -0700

    Adds Car object

--- js/data_structures.js
+++ js/data_structures.js
@@ -11,8 +11,39 @@ console.log(colors);
 console.log(names);
 
 var horses ={}
+
 for (var i = 0; i < names.length; i++){
 	horses[names[i]] = colors[i];
 }
 
 console.log(horses)
+
+//
+
+
+function Car(type, color, fast) {
+	console.log("Our new car:", this);
+
+	this.type = type;
+	this.color = color;
+	this.fast = fast;
+
+	this.honk = function() { console.log("HONK!"); };
+	
+	console.log("CAR INITIALIZATION COMPLETE");
+}
+
+console.log("Let's build a car ...");
+
+var anotherCar = new Car("Volvo", "red", false);
+console.log(anotherCar)
+console.log("Our car can honk:");
+anotherCar.honk();
+console.log("----");
+
+console.log("Let's build another car ...");
+var yetAnotherCar = new Car("Lamborgini", "black", true);
+console.log(yetAnotherCar.type);
+console.log("This new car can honk, too:");
+yetAnotherCar.honk();
+console.log("----")
