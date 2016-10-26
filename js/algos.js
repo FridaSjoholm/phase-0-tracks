1e3e93b5eaf4af9e8a9d8db13648e15ff88948c3
FridaSjoholm <frida@fridaesign.se>
2016-10-26 10:50:31 -0700

    initial commit, pseudocode

---
+++ js/algos.js
@@ -0,0 +1,5 @@
+// To solve this we need to make a method that takes an array of strings as input, 
+//Then we'll set a variable x that stores the longest string so far. 
+//- Make a loop that loops through each string in array and compares it to x, if its longer
+// x will be given value of string (first time, it definitely will be as x is set to a empty string)
+// After loop retrurn longest word.
