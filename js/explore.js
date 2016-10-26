a6d9c5ffecb4298324de7dd0e9bb392c343e639e
FridaSjoholm <frida@fridaesign.se>
2016-10-26 10:16:13 -0700

    Makes reversing the string into a function

--- js/explore.js
+++ js/explore.js
@@ -2,16 +2,12 @@
 // Iterate through string in reversed order, from back to front and  for each new letter, 
 //add it to string for reversed reversed word
 
-
-var reversed_string = ""
-
-
-
-console.log("give us a word, please!")
-var original_word = gets.chomp
-
-for (var i = original_word.length-1; i >= 0; i-=1){
-	reversed_string += original_word[i]
+function reverseString(originalWord){
+	var reversedString = ""
+	for (var i = originalWord.length-1; i >= 0; i-=1){
+		reversedString += originalWord[i]
+	}
+	return reversedString
 }
-
-console.log(reversed_string)
+console.log(reverseString("whatever"))
+console.log(reverseString("not a palindrome"))
