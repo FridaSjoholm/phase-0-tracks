1fb3c8b27011c2e079453a761296481add8361d0
FridaSjoholm <frida@fridaesign.se>
2016-10-26 10:51:03 -0700

    Adds working function

--- js/algos.js
+++ js/algos.js
@@ -3,3 +3,16 @@
 //- Make a loop that loops through each string in array and compares it to x, if its longer
 // x will be given value of string (first time, it definitely will be as x is set to a empty string)
 // After loop retrurn longest word.
+
+
+function comparer(stringArray){
+	var longest = ""
+	for (var i = 0; i < stringArray.length; i++){
+		if (stringArray[i].length> longest.length){
+			longest = stringArray[i]
+		}
+	}
+	return longest
+}
+
+console.log(comparer(["long phrase","longest phrase","longer phrase"]))
